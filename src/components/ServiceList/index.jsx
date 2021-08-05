728a6bdfaa66d93252d0e1136abea497ec4aef89
AlLevykin <a.a.levykin@gmail.com>
2021-08-05 19:39:58 +0500

    1 file modified

--- src/components/ServiceList/index.jsx
+++ src/components/ServiceList/index.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import {nanoid} from "nanoid";
 
-const ServiceList = (props) => <h3>ServiceList</h3>;
+const ServiceList = (props) => {
+
+  const items = [
+    {
+      id: nanoid(),
+      name: "Замена стекла",
+      price: 22.50
+    },
+    {
+      id: nanoid(),
+      name: "Замена материнской платы",
+      price: 159.99
+    }
+  ];
+
+  return (
+    (items.length > 0) ? 
+      <h3>ServiceList</h3>
+    :
+      <></>
+    );
+  }
 
 export default ServiceList;
