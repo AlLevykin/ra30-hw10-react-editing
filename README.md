d23b013e184990e0586e4b80caaca046d3fb1e0d
AlLevykin <a.a.levykin@gmail.com>
2021-08-05 10:58:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xtgsir
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xtgsir)
---
+++ src/components/App/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function App() {
+return (
+<div>
+<h1 className="text-danger">Hello StackBlitz!</h1>
+<p>Start editing to see some magic happen :)</p>
+</div>
+);
+}
