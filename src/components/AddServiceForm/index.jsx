a0d1171165353775c4e5b839c7102c35b726e5cc
AlLevykin <a.a.levykin@gmail.com>
2021-08-05 14:34:50 +0500

    1 file modified

--- src/components/AddServiceForm/index.jsx
+++ src/components/AddServiceForm/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const AddServiceForm = (props) => <h3>form</h3>;
+const AddServiceForm = (props) => {
+
+  return (
+    <h3>form</h3>
+  );
+
+}
 
 export default AddServiceForm;
 
