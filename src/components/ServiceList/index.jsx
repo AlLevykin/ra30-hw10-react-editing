5a555a7670447c957826e0ce34a1957410ea6cac
AlLevykin <a.a.levykin@gmail.com>
2021-08-05 20:51:17 +0500

    1 file modified

--- src/components/ServiceList/index.jsx
+++ src/components/ServiceList/index.jsx
@@ -1,36 +1,40 @@
-import React from "react";
-import {nanoid} from "nanoid";
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Trash, Pen } from 'react-bootstrap-icons';
 
-const ServiceList = (props) => {
-
+const ServiceList = props => {
   const items = [
     {
       id: nanoid(),
-      name: "Замена стекла",
-      price: 22.50
+      name: 'Замена стекла',
+      price: 22.5
     },
     {
       id: nanoid(),
-      name: "Замена материнской платы",
+      name: 'Замена материнской платы',
       price: 159.99
     }
   ];
 
-  return (
-    (items.length > 0) ? 
-      <ul className="list-group list-group-flush">
-        {
-          items.map((item) =>
-            <li className="list-group-item" key={item.id}>
-              <span>{item.name} {item.price}</span>
-              <button type="button" className="btn btn-danger mx-3"><X /></button>
-            </li>
-          )
-        }
-      </ul>
-    :
-      <></>
-    );
-  }
+  return items.length > 0 ? (
+    <ul className="list-group list-group-flush">
+      {items.map(item => (
+        <li className="list-group-item" key={item.id}>
+          <span>
+            {item.name} {item.price}
+          </span>
+          <button type="button" className="btn btn-success ms-3">
+            <Pen />
+          </button>
+          <button type="button" className="btn btn-danger ms-3">
+            <Trash />
+          </button>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <></>
+  );
+};
 
 export default ServiceList;
