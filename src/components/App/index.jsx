bb947e79bcc2a19910e03ad66c877ff8f5b7ff03
AlLevykin <a.a.levykin@gmail.com>
2021-08-06 16:57:16 +0500

    3 files modified

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ServiceForm from "../ServiceForm";
-import ServiceList from '../ServiceList';
+import ServiceList from "../ServiceList";
 
 export default function App() {
 return (
