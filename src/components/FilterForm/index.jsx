ba85d18cd7c798b85e9a4d74c217c41b964f8dc0
AlLevykin <a.a.levykin@gmail.com>
2021-08-07 11:41:34 +0500

    1 file modified

--- src/components/FilterForm/index.jsx
+++ src/components/FilterForm/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Funnel, XCircle } from 'react-bootstrap-icons';
+import { filterServiceList } from '../../actions/actionCreators';
 
 const FilterForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(filterServiceList('стекл'));
+  };
+
+  const handleClearFilter = () => dispatch(filterServiceList(''));
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="input-group">
-        <input type="text" className="form-control" />
+        <input type="text" name="filter" className="form-control" />
         <button className="btn btn-primary" type="submit">
           <Funnel />
         </button>
-        <button className="btn btn-danger">
+        <button className="btn btn-danger" onClick={handleClearFilter}>
           <XCircle />
         </button>
       </div>
